f78f7fa7b12255d0dee1148a3f2977b7a3785db1
nicco <npiazzesi@gmail.com>
2021-03-14 21:26:59 +0100

    continued problem description

--- code/job_sequencing.cpp
+++ code/job_sequencing.cpp
@@ -8,13 +8,18 @@ struct Job
 {
     std::string id;
     int profit, deadline;
-    Job(std::string id, int profit, int deadline) : id(id), profit(profit), deadline(deadline) {}
+    Job(std::string id, int profit, int deadline) : 
+    id(id), profit(profit), deadline(deadline) {}
 };
 
 std::vector<Job> jobSequencing(std::vector<Job> jobs)
 {
-    std::sort(jobs.begin(), jobs.end(), [](Job j1, Job j2) { return j1.profit > j2.profit; });
-    int maxD = std::max_element(jobs.begin(), jobs.end(), [](Job j1) { return j1.deadline; })->deadline;
+    std::sort(jobs.begin(), jobs.end(), 
+            [](Job j1, Job j2) { return j1.profit > j2.profit; });
+    
+    int maxD = std::max_element(jobs.begin(), jobs.end(), 
+            [](Job j1) { return j1.deadline; })->deadline;
+    
     std::vector<Job> sol;
     UnionFind uf(maxD + 1);
     for (Job j : jobs)
